4df1e856c0c169d41526e0a1ca430ce3e73a0ca6
manu <manu.bellemans@gmail.com>
2021-07-27 00:38:40 +0200

    add built frontend + endpoint

---
+++ src/build/precache-manifest.6e8da6d441818af994fd0b2d60257043.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b03a61988f03cb42003e",
+    "url": "/static/css/main.44380db8.chunk.css"
+  },
+  {
+    "revision": "b03a61988f03cb42003e",
+    "url": "/static/js/main.cb6e85fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "88772428cd11b0d5a076",
+    "url": "/static/js/2.7ec22fe6.chunk.js"
+  },
+  {
+    "revision": "d234fd45365a936b57bc81adfb6cca5b",
+    "url": "/index.html"
+  }
+];
